3d0e89f462feddc6114e91d5e49c9d7365c0ede1
nitishsingh41 <45527813+nitishsingh41@users.noreply.github.com>
2024-07-02 18:07:28 +0530

    Create install_libraries.py

---
+++ install_libraries.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+
+# Set environment variables
+os.environ['CMAKE_ARGS'] = "-DLLAMA_CUBLAS=on"
+os.environ['FORCE_CMAKE'] = "1"
+
+# Run pip install for requirements.txt
+subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
