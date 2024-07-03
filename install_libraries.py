8e9f8a3a9cac9a436d2df1d1056592d6b0abaad0
nitishsingh41 <45527813+nitishsingh41@users.noreply.github.com>
2024-07-03 15:26:23 +0530

    Update install_libraries.py

--- install_libraries.py
+++ install_libraries.py
@@ -1,7 +1,7 @@
 import os
 
 
-# Set environment variables
+# Set environment variables and install llama_cpp_python for cuda
 os.system('CMAKE_ARGS="-DLLAMA_CUDA=on" FORCE_CMAKE=1 pip install llama-cpp-python')
 # Run pip install for requirements.txt
 os.system("pip install -r requirements.txt")
