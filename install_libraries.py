0a40f282a2275e89f3a13793fdf9df2c1bcb681f
nitishsingh41 <45527813+nitishsingh41@users.noreply.github.com>
2024-07-03 13:06:20 +0530

    Update install_libraries.py

--- install_libraries.py
+++ install_libraries.py
@@ -2,7 +2,6 @@ import os
 
 
 # Set environment variables
-os.system('export CMAKE_ARGS="-DLLAMA_CUBLAS=on"')
-os.system('export FORCE_CMAKE="1"')
+os.system('CMAKE_ARGS="-DLLAMA_CUDA=on" FORCE_CMAKE=1')
 # Run pip install for requirements.txt
 os.system("pip install -r requirements.txt")
